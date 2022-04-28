c32d2e19dab813089a75ba159e1f1b00e7426252
adri-leiva <adriana.leiva@est.fi.uncoma.edu.ar>
2022-04-28 19:14:09 -0300

    Separe el header y el footer en archivos php

---
+++ vista/structure/header.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>GaTop</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+  <link rel="stylesheet" href="../estilos/index.css">
+  <link rel="stylesheet" href="../estilos/ej-11.css">
+ 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
+
+</head>
+
+<body>
+  <!-- Header -->
+  <header class="container w-75 mx-auto mt-3">
+    <div class="row">
+      <a href="./ej-0.html"
+        class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center title">
+        ropa para gatos</a>
+      <a href="./ej-0.html"
+        class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center logo">
+        <img src="../assets/Logo.png" alt="logo del grupo">
+      </a>
+      <a href="./ej-0.html"
+        class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center title">
+        felicidad para vos</a>
+    </div>
+  </header>
+    <!-- Separator -->
+    <div class="separador w-75 border border-1 mx-auto my-3"></div>
+
+<!-- Nav -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light w-50 mx-auto rounded-3">
+  <div class="container-fluid">
+    <ul class="navbar-nav w-100 nav justify-content-around">
+      <li class="nav-item">
+        <a class="nav-link custom-link" aria-current="page" href="../tp2/tp2-ej0.php">Inicio</a>
+      </li>
+      <li class="nav-item dropdown ">
+        <a class="nav-link custom-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+          data-bs-toggle="dropdown" aria-expanded="false">
+          Bootstrap
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <li class="nav-item">
+        <a class="nav-link custom-link" aria-current="page" href="../ej-0-popper.html">popper?</a>
+      </li>
+          <li><a class="dropdown-item" href="../ej-1.html">ejercicio 1</a></li>
+          <li><a class="dropdown-item" href="../ej-2.html">ejercicio 2</a></li>
+          <li><a class="dropdown-item" href="../ej-3.html">ejercicio 3</a></li>
+          <li><a class="dropdown-item" href="../ej-4.html">ejercicio 4</a></li>
+          <li><a class="dropdown-item" href="../ej-5.html">ejercicio 5</a></li>
+          <li><a class="dropdown-item" href="../ej-6.html">ejercicio 6</a></li>
+          <li><a class="dropdown-item" href="../ej-7.html">ejercicio 7</a></li>
+          <li><a class="dropdown-item" href="../ej-8.html">ejercicio 8</a></li>
+          <li><a class="dropdown-item" href="../ej-9.html">ejercicio 9</a></li>
+          <li><a class="dropdown-item" href="../ej-10.html">ejercicio 10</a></li>
+          <li><a class="dropdown-item" href="../ej-11.html">ejercicio 11</a></li>
+        </ul>
+      </li>
+
+      <li class="nav-item dropdown ">
+        <a class="nav-link custom-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+          data-bs-toggle="dropdown" aria-expanded="false">
+          Ajax &AMP; jquery
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <li><a class="dropdown-item" href="#">Ejercicio 1</a></li>
+          <li><a class="dropdown-item" href="#">Ejercicio 2</a></li>
+         
+        </ul>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+
+<!-- Content -->
+<div class="container row main-content mx-auto align-items-center my-5 p-3 w-75">
+  <div class="container dominio p-5 row main-content mx-auto align-items-center">
+    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 
+      text-xxl-end text-cl-end text-lg-end text-md-center text-sm-center text-xs-center
+      content-text">
+      <p>
+        Imagina tu gato con <span class="sacramento">corbata.</span>
+        <br> Seguro le vendria bien una <span class="sacramento">bata!</span>
+        <br> Y si usara <span class="sacramento">alpargatas?</span>
+        <br><br>
+      </p>
+      <p class="pt-3 content-text_secondary">
+        Tus niveles de estres disminuiran exponencial y correlativamente al aumento de tu felicidad,
+        pensalo.
+      </p>
+    </div>
+    <div
+      class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 overflow-hidden d-flex justify-content-center">
+      <img id="gato-1" class="gato" src="../assets/gato-corbata.png" alt="gato corbata">
+      <img id="gato-2" style="display: none; opacity: 0" class="gato" src="../assets/gato-bata.png" alt="gato bata">
+      <img id="gato-3" style="display: none; opacity: 0" class="gato" src="../assets/gato-comfy.png" alt="gato comfy">
+    </div>
+  </div>
+</div>
