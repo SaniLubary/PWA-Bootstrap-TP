c4bcfd21a99e7f2278cc0aef6763cfa78e417c51
SaniLubary <santiago.lp.cop@gmail.com>
2022-05-10 09:31:25 -0300

    resolvimos ej 1 selects con jquery

--- vista/js/tp2-ej2.js
+++ vista/js/tp2-ej2.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+$(document).ready(async function () {
+  // Obtenemos y carganmos las tabs para el ejercicio 2
   fetch('http://localhost:8001/requests/tab')
     .then(response => response.json())
     .then(data => {
@@ -6,4 +7,35 @@ $(document).ready(function () {
       $('#pills-razones').append(data.payload[1].contenido);
     })
     .catch(error => console.error(error));
+
+  // Get estaciones
+  const estaciones = await fetch('http://localhost:8001/requests/estacion')
+    .then(response => response.json())
+    .then(data => {
+      return data.payload
+    })
+    .catch(error => console.error(error));
+
+  // Get categorias
+  const categorias = await fetch('http://localhost:8001/requests/categoria')
+    .then(response => response.json())
+    .then(data => {
+      return data.payload
+    })
+    .catch(error => console.error(error));
+
+  // Cargando estaciones en el select
+  estaciones.map(estacion => {
+    $('#estaciones').append(`<option value="${estacion.id}">${estacion.descripcion}</option>`)
+  })
+
+  $("#estaciones").change(function () {
+    const idEstacion = $('#estaciones').find(":selected").val();
+    const categoriasFiltrado = categorias.filter(categoria => categoria.estacion.id == idEstacion)
+
+    // Cargando estaciones en el select
+    categoriasFiltrado.map(categoria => {
+      $('#categorias').append(`<option value="${categoria.id}">${categoria.descripcion}</option>`)
+    })
+  });
 });
---
+++ vista/tp-ajax-jquery/ej-1.php
@@ -0,0 +1,20 @@
+<?php include_once("../components/header.php"); ?>
+
+<!-- Content -->
+<div class="container row main-content mx-auto align-items-center my-5 p-3 w-75">
+
+  <div class="container dominio">
+    <select class="form-select" id="estaciones" aria-label="Default select example">
+      <option selected>Seleccionar estacion del año</option>
+    </select>
+
+    <select class="form-select" id="categorias" aria-label="Default select example">
+      <option selected>Open this select menu</option>
+    </select>
+
+  </div>
+</div>
+
+<script src="../js/tp2-ej2.js" defer></script>
+
+<?php include_once("../components/footer.php"); ?>
