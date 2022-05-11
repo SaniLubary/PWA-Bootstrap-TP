86c16fb758286452295b95cd6a409b0f2fe7cf32
SaniLubary <santiago.lp.cop@gmail.com>
2022-05-10 21:18:35 -0300

    update links header

--- vista/components/header.php
+++ vista/components/header.php
@@ -23,12 +23,12 @@
   <!-- Header -->
   <header class="container w-75 mx-auto mt-3">
     <div class="row">
-      <a href="../tp-bootstrap/ej-0.html" class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center title">
+      <a href="/vista/tp-ajax-jquery" class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center title">
         ropa para gatos</a>
-      <a href="../tp-bootstrap/ej-0.html" class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center logo">
+      <a href="/vista/tp-ajax-jquery" class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center logo">
         <img src="../assets/Logo.png" alt="logo del grupo">
       </a>
-      <a href="../tp-bootstrap/ej-0.html" class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center title">
+      <a href="/vista/tp-ajax-jquery" class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xs-12 d-flex justify-content-center align-items-center title">
         felicidad para vos</a>
     </div>
   </header>
@@ -40,7 +40,7 @@
     <div class="container-fluid">
       <ul class="navbar-nav w-100 nav justify-content-around">
         <li class="nav-item">
-          <a class="nav-link custom-link" aria-current="page" href="../tp2/tp2-ej0.php">Inicio</a>
+          <a class="nav-link custom-link" aria-current="page" href="/vista/tp-ajax-jquery">Inicio</a>
         </li>
         <li class="nav-item dropdown ">
           <a class="nav-link custom-link" href="/vista/tp-bootstrap/ej-0.html" role="button">
@@ -55,6 +55,7 @@
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
             <li><a class="dropdown-item" href="ej-1.php">Ejercicio 1</a></li>
             <li><a class="dropdown-item" href="ej-2.php">Ejercicio 2</a></li>
+            <li><a class="dropdown-item" href="ej-3-ex11.php">Ejercicio 2</a></li>
           </ul>
         </li>
 
