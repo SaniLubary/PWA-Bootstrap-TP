6d2e37b5ffe1edb2c322be1039c5045ecc799e72
SaniLubary <santiago.lp.cop@gmail.com>
2022-05-11 22:52:41 -0300

    fix ajax ej4

--- vista/js/tp2-ej4.js
+++ vista/js/tp2-ej4.js
@@ -1,68 +1,76 @@
-$(function() {
-
+$(function () {
 	'use strict';
 
 	// Form
-
-	var contactForm = function() {
-
-		if ($("#contactForm").length > 0 ) {
-
-			$( "#contactForm" ).validate( {
-				
-				rules: {
-					nombre:{
-						required: true,
-						minlength: 10
-					},
-					empresa: {
-						required: true,
-						minlength: 10
-					},
-					telefono: {
-						required: true,
-						minlength: 10
-					},
-					mail: {
-						required: true,
-						email: true
-					},
-					comentario: {
-						required: true,
-						minlength: 10
-					}
+	var contactForm = function () {
+		$("#contactForm").validate({
+			rules: {
+				nombre: {
+					required: true,
+					maxlength: 10
+				},
+				empresa: {
+					required: true,
+					maxlength: 10
+				},
+				telefono: {
+					required: true,
+					maxlength: 10
+				},
+				mail: {
+					required: true,
+					email: true
 				},
 				comentario: {
-					nombre: "Ingrese su nombre",
-					mail: "Ingrese una direccion de correo valida",
-					comentario: "Ingrese un comentario"
-				},
-				
-			} );
-		}
-		
-	};
-	async function submit() {
-		const rawResponse = await fetch('http://localhost:8001/requests/contactos', {
-		  method: 'POST',
-		  headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify({
-			nombre: 'data del submit',
-			empresa: 'data del submit',
-			telefono: 'data del submit',
-			mail: 'data del submit',
-			comentario: 'data del submit'
-		  })
+					required: true,
+					maxlength: 50
+				}
+			},
+			comentario: {
+				nombre: "Ingrese su nombre",
+				empresa: "Indique una empresa",
+				telefono: "Indique un telefono valido xxx-xxx-xxxx",
+				mail: "Ingrese una direccion de correo valida",
+				comentario: "Ingrese un comentario"
+			},
+
 		});
-		
-		const content = await rawResponse.json();
-	  
-		console.log(content);
-	  };
+	};
+
+	// Iniciamos la validacion del formulario
 	contactForm();
 
-	 
+	// Enviamos respuesta on submit al servidor
+	$("#contactForm").submit(function (event) {
+		event.preventDefault();
+
+		if ($("#contactForm").validate()) {
+			submit()
+		}
+
+	});
+
+
 });
+
+async function submit() {
+	const rawResponse = await fetch('http://localhost:8001/requests/contacto', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			nombre: $('#nombre').val(),
+			empresa: $('#empresa').val(),
+			telefono: $('#telefono').val(),
+			mail: $('#mail').val(),
+			comentario: $('#comentario').val()
+		})
+	});
+
+
+
+	const content = await rawResponse.json();
+	console.log(content);
+};
--- vista/tp-ajax-jquery/ej-4.php
+++ vista/tp-ajax-jquery/ej-4.php
@@ -1,5 +1,6 @@
 <?php include_once("../components/header.php"); ?>
-<link rel="stylesheet" href="../../vista/css/style_tp2ej4.css"> </link>
+<link rel="stylesheet" href="../../vista/css/style_tp2ej4.css">
+</link>
 
 <!-- Content -->
 <div class="container row main-content mx-auto align-items-center my-5 p-3 w-75">
@@ -14,12 +15,12 @@
                                 <div class="col-md-6">
                                     <h3 class="heading mb-4">Nuevo usuario</h3>
                                     <p>Completa con tus datos para formar parte.</p>
-                                    <p> <img src="../assets/tp2-ej4-images/new_user.jpg" alt="Image" class="img-fluid"> </p> 
+                                    <p> <img src="../assets/tp2-ej4-images/new_user.jpg" alt="Image" class="img-fluid"> </p>
                                 </div>
-                                
+
                                 <div class="col-md-6">
                                     <!-- Formulario -->
-                                    <form class="mb-5" method="post" id="contactForm" name="contactForm">
+                                    <form class="mb-5" id="contactForm" name="contactForm">
                                         <div class="row">
                                             <div class="col-md-12 form-group">
                                                 <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre completo">
@@ -37,19 +38,19 @@
                                                 <input type="tel" pattern="^\d{4}-\d{3}-\d{4}$" class="form-control" name="telefono" id="telefono" placeholder="Telefono">
                                             </div>
                                         </div>
-                                        
+
                                         <div class="row">
                                             <div class="col-md-12 form-group">
                                                 <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" class="form-control" name="mail" id="mail" placeholder="Mail">
                                             </div>
                                         </div>
-                                        
+
                                         <div class="row">
                                             <div class="col-md-12 form-group">
                                                 <textarea class="form-control" name="comentario" id="comentario" cols="30" rows="7" placeholder="Comentarios"></textarea>
                                             </div>
-                                        </div>  
-                                        
+                                        </div>
+
                                         <div class="row">
                                             <div class="col-12">
                                                 <input id="btn_enviar" type="submit" value="Enviar formulario" class="btn btn-primary rounded-0 py-2 px-4">
@@ -58,29 +59,29 @@
                                         </div>
                                     </form>
 
-              <div id="form-message-warning mt-4"></div> 
-              <div id="form-message-success">
-                Your message was sent, thank you!
-              </div>
+                                    <div id="form-message-warning mt-4"></div>
+                                    <div id="form-message-success">
+                                        Your message was sent, thank you!
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-          </div>
+
+
+
+
+
+            <script src="../js/jquery.validate.min.js"></script>
+            <script src="../js/tp2-ej4.js"></script>
+
         </div>
-      </div>
     </div>
+</div>
 
-  </div>
-    
-    
-
-    
-   
-    <script src="../js/jquery.validate.min.js"></script>
-    <script src="../js/tp2-ej4.js"></script>
-
-      </div>
-    </div>
-  </div>
-  
 </div>
 
 <!-- <script src="../js/tp2-ej2.js" defer></script> -->
